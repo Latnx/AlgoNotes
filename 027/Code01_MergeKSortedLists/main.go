513ad5076d54487ea8c8fcb8df216b8caecfdc26
Tequila <shihaoyue36@gmail.com>
2025-05-20 18:24:07 +0800

    堆结构常见问题

---
+++ 027/Code01_MergeKSortedLists/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "container/heap"
+
+// https://leetcode.cn/problems/merge-k-sorted-lists
+// 合并 K 个升序链表
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type hp [](*ListNode)
+
+func (h hp) Len() int           { return len(h) }
+func (h hp) Less(i, j int) bool { return h[i].Val < h[j].Val }
+func (h hp) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *hp) Push(x any)        { (*h) = append(*h, x.(*ListNode)) }
+func (h *hp) Pop() any          { x := (*h)[len(*h)-1]; *h = (*h)[:len(*h)-1]; return x }
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	var h hp
+	for index, list := range lists {
+		if list != nil {
+			h.Push(list)
+			lists[index] = list.Next
+		}
+	}
+	if h.Len() == 0 {
+		return nil
+	}
+	heap.Init(&h)
+	head := heap.Pop(&h).(*ListNode)
+	p := head
+	for len(h) > 0 {
+		if p.Next != nil {
+			heap.Push(&h, p.Next)
+		}
+		p.Next = heap.Pop(&h).(*ListNode)
+		p = p.Next
+	}
+	return head
+}
+
+func main() {
+	mergeKLists([]*ListNode{})
+}
---
+++ 027/Code03_MinimumOperationsToHalveArraySum/main.go
@@ -0,0 +1,37 @@
+package main
+
+// https://leetcode.cn/problems/minimum-operations-to-halve-array-sum
+// 将数组和减半的最少操作次数
+
+import "container/heap"
+
+type hp []int
+
+func (h hp) Len() int           { return len(h) }
+func (h hp) Less(i, j int) bool { return h[i] > h[j] }
+func (h hp) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *hp) Push(x any) { (*h) = append(*h, x.(int)) }
+func (h *hp) Pop() any   { x := (*h)[len(*h)-1]; (*h) = (*h)[:len(*h)-1]; return x }
+
+func halveArray(nums []int) int {
+	sum := 0
+	n := 0
+	var h hp = make([]int, len(nums))
+	for i, num := range nums {
+		h[i] = num << 20
+		sum += h[i]
+	}
+	heap.Init(&h)
+	subtotal := 0
+	for ; sum/2 > subtotal; n++ {
+		h[0] /= 2
+		subtotal += h[0]
+		heap.Fix(&h, 0)
+	}
+	return n
+}
+
+func main() {
+	halveArray([]int{})
+}
