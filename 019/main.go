3b65d2eacf608661f1d1dfba9cf2ca57cb861de2
Tequila <shihaoyue36@gmail.com>
2025-10-13 15:25:12 +0800

    输入输出

--- 019/main.go
+++ 019/main.go
@@ -5,19 +5,23 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n') // 读取整行输入
-	input = strings.TrimSpace(input)
-	// 去除换行符和空格
-	// 解析整数数组
-	numStrs := strings.Split(input, " ")
-	nums := make([]int, len(numStrs))
-	for i, str := range numStrs {
-		nums[i], _ = strconv.Atoi(str)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	arr := make([]int, n)
+	for i := range n {
+		scanner.Scan()
+		arr[i], _ = strconv.Atoi(scanner.Text())
 	}
-	fmt.Println("Parsed numbers:", nums)
+	for i := range n {
+		fmt.Fprintln(writer, arr[i])
+	}
 }
